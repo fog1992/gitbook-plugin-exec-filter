7fe06a4f2df2536210a7c3de17ff2b84798ec0bb
Nick Santamaria <nick.santamaria@previousnext.com.au>
2018-02-05 16:21:37 +1100

    Added newline trimming to command output.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const execSync = require('child_process').execSync;
 const trim = require('trim-whitespace');
+const trimNewlines = require('trim-newlines');
 
 module.exports = {
   filters: {
     command: function(command) {
       var output = execSync(command).toString();
+      output = trimNewlines(output);
       return trim(output);
     }
   }
